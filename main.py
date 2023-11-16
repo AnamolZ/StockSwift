92f6dcbecb9c514460218538af6eb50a62bd8274
Anamol <anmoldkl971@gmail.com>
2023-11-16 14:44:16 +0545

    version 1.0

--- main.py
+++ main.py
@@ -25,4 +25,17 @@ def get_stock_prices():
     symbols = ["AAPL", "AMZN", "GOOGL", "MSFT", "TSLA", "GOOG", "FB", "NVDA", "BA", "NFLX"]
     stock_data = {}
 
-    
+    for symbol in symbols:
+        try:
+            stock = yf.Ticker(symbol)
+            data = stock.history(period="1d")
+            last_price = round(data["Close"].iloc[-1], 2)
+            history = [round(price, 2) for price in data["Close"].tolist()]
+            stock_data[symbol] = {
+                "symbol": symbol,
+                "price": last_price,
+                "history": history,
+            }
+        except Exception as e:
+            logging.error(f"Error fetching data for {symbol}: {e}")
+                
