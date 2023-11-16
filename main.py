ea81dc5c7ecaeaf1b622ad0046d3f5002dae2867
Anamol <anmoldkl971@gmail.com>
2023-11-16 14:41:25 +0545

    version 1.0

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import asyncio
+import logging
+import json
+
 from fastapi import FastAPI, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 
+app = FastAPI()
+
+app.add_middleware(
