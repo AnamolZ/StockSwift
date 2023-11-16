be7844bb29e88695704c97a9803f18681cea2fa0
Anamol <anmoldkl971@gmail.com>
2023-11-16 15:20:19 +0545

    version 1.0

--- main.py
+++ main.py
@@ -164,4 +164,10 @@ async def websocket_endpoint(websocket: WebSocket):
         logging.error(f"WebSocket connection error: {e}")
         raise
 """
+    Instructions:
+    - Ensure that all necessary dependencies are installed (use requirements.txt or similar).
+    - Start the FastAPI application using the following command:
+        uvicorn your_module_name:app --reload
+    - Access the API documentation at http://127.0.0.1:8000/docs for interactive documentation.
+    - Visit http://127.0.0.1:8000/ in your browser to access the main application.
 """
