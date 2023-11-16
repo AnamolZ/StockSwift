f2f2ba095551e2a64a0af9d3c697439338b4afcd
Anamol <anmoldkl971@gmail.com>
2023-11-16 14:43:54 +0545

    version 1.0

--- main.py
+++ main.py
@@ -22,4 +22,7 @@ logging.basicConfig(filename='error.log', level=logging.ERROR)
 app.mount("/static", StaticFiles(directory="./static"), name="static")
 
 def get_stock_prices():
+    symbols = ["AAPL", "AMZN", "GOOGL", "MSFT", "TSLA", "GOOG", "FB", "NVDA", "BA", "NFLX"]
+    stock_data = {}
+
     
